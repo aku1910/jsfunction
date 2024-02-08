af65d1623c91b4e0b8d71f68370671cf4bacb8eb
aku1910 <154792762+aku1910@users.noreply.github.com>
2024-02-07 17:37:26 -0800

    first commit

---
+++ js.js
@@ -0,0 +1,66 @@
+//task1
+// let topla=0 ;
+// let cix=0 ;
+// let vur=1;
+// let bol=1;
+// let emel="vurma";
+// function toplama(num1,num2,caltopla) {
+//     topla=num1+num2;
+//     return caltopla(topla);
+// }
+// function cixma(num1,num2,calminus) {
+//     cix=num1-num2
+//     return calminus(cix)
+// }
+// function bolme(num1,num2,calbolme){
+//     bolme=num1/num2
+//     return calbolme(bolme)
+// }
+
+// function vurma(num1,num2,calvurma){
+//     vurma=num1*num2
+//     return calvurma(vurma)
+// }
+
+// function Output(output){
+//   console.log(output);
+// }
+
+// switch (emel) {
+//     case "topla":
+//         toplama(15,5,Output)
+//      break;
+//      case "cix":
+//        cixma(15,5,Output)
+//      break;
+//      case "bolme":
+//        bolme(15,5,Output)
+//         break;
+//         case "vurma":
+//             vurma(15,5,Output)
+//             break;
+//             default:
+//         console.log("emel deyil")
+//         break;
+// }
+
+
+// task 2
+
+// let lastarr = [12, 34, 25, 15, 30, 75];
+// function TekEded(lastarr,callback) {
+    
+//     let arr= lastarr.filter(x=>x%2==!0)
+//     return callback (arr,Output);
+    
+// }
+// function onbes(lastarr,onbescallback) {
+//     arr= lastarr.filter(x=>x%15==0)
+//     return onbescallback(arr)
+// }
+// function Output(output) {
+//     console.log(output);
+// }
+
+// TekEded(lastarr,onbes)
+
